08a765f082cb66387a2cbb66a7154050a882a09e
yoojin <yoojin9005@gmail.com>
2022-07-01 12:49:09 -0400

    commit tree 701530af2b8ba4a552409b761bdf53b22d3f17bb

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+//   };
+
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(res => res.json())
+//   .then(object => console.log(object))
+//   .catch(function (error) { 
+//     alert("Bad things!");
+//     console.log(error.message);
+//   })
+
+
+
+
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+      },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(res => res.json())
+    .then( e => {
+        document.body.append(e ["id"])
+    })
+
+        // document.body.innerHTML = object[ "id" ]})
+
+
+    .catch(error => {
+        document.body.append(error.message)
+    })
+}
